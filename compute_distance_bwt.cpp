5f9481026c7ccbd03a201b4bc28d424d2594e68c
alesantos709 <tuoemail@example.com>
2025-02-25 10:08:48 +0100

    modifica gitignore

--- compute_distance_bwt.cpp
+++ compute_distance_bwt.cpp
@@ -13,3 +13,13 @@ for (const auto &elem : gda_read) {
     printf("EDS: %d, Dollaro: %d\n", elem.eds_index, elem.dollar_index);
 }
 */
+
+/*
+- aprire la bwt e caricarla in un array di char
+- aprire il gda e caricarlo in un array di gda_element
+- fare un ciclo for annidato che:
+    - carica in un altro array di char gli elementi appartenenti a due stringhe
+    - conta le alternanze di colori
+    - salva il valore in una matrice
+- salva la matrice in un file (vedere formato, forse phylip che è lo standard in bionformatica)
+*/
